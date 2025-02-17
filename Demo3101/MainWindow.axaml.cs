8849b079da09031dbac12bc7863b508c24d45631
KLim <jjj>
2025-02-17 17:20:43 +0300

    new steps//17.02.2025

--- Demo3101/MainWindow.axaml.cs
+++ Demo3101/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Styling;
 using Demo3101.Context;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -14,8 +16,47 @@ namespace Demo3101
             adminHoz.Click += AdminHoz_Click;
             toFirst.Click += ToFirst_Click;
             toMain.Click += ToMain_Click;
+            toSecondDep.Click += ToSecondDep_Click;
+            toMain2.Click += ToMain2_Click;
+            toThirdDep.Click += ToThirdDep_Click;
+            mainButton.Click += MainButton_Click;
             SetData();
         }
+
+        private void MainButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            mainButton.Background = Brushes.Green;
+            SolidColorBrush solidColorBrush = new SolidColorBrush(Color.Parse("#e1f4c8"));
+            toFirstDep.Background = solidColorBrush;
+            toSecondDep.Background = solidColorBrush;
+            toThirdDep.Background = solidColorBrush;
+            toFourthDep.Background = solidColorBrush;
+            toFifthDep.Background = solidColorBrush;
+            toSixthDep.Background = solidColorBrush;
+            toSeventhDep.Background = solidColorBrush;
+            toEighthDep.Background = solidColorBrush;
+            toNinthDep.Background = solidColorBrush;
+            toTenthDep.Background = solidColorBrush;
+            toEleventhDep.Background = solidColorBrush;
+            toTwelfthDep.Background = solidColorBrush;
+        } 
+
+        private void ToThirdDep_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            SolidColorBrush solidColorBrush = new SolidColorBrush(Color.Parse("#e1f4c8"));
+            toThirdDep.Background = Brushes.Green;
+            mainButton.Background = solidColorBrush;
+        } 
+
+        private void ToMain2_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            secondDep.IsVisible = false;
+        } 
+
+        private void ToSecondDep_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            secondDep.IsVisible = true;
+        } 
 
         private void ToMain_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
